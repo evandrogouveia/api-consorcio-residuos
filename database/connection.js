354676c400d8fc6e54b6897dd6b0099d8d182c07
vandogouveia <vando_gouveia@hotmail.com>
2023-02-23 15:50:45 -0300

    commit

--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     multipleStatements: true,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER_NAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0
 });
 
 connection.connect(function (err) {
