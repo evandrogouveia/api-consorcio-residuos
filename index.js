17ab38723afc389b88afaa0cc8a14a4b3b610e43
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 14:41:12 -0300

    update agent

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
+const allowedOrigins = [
+    'http://localhost:4200',
+    'https://api-camara.vercel.app'
+];
 
-app.use(cors({
-    origin: '*',
-}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
-app.use(bodyParser.json({limit: '50mb'}));
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origin not allowed by CORS'));
+        }
+    },
+};
+app.options('*', cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 
 const router = require('./src/routes');
 app.use(router);
-app.use('/uploads', cors(), express.static('uploads'));
+app.use('/uploads', cors(corsOptions), express.static('uploads'));
 
-app.use('/', cors(), router);
+app.use('/', cors(corsOptions), router);
 
 app.listen(port);
 console.log('API funcionando!');
