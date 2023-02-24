99b7884c7481266e14cbe3f23361d99925842eb4
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 15:02:57 -0300

    update agent

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 dotenv.config();
 
 let allowCors = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "https://projeto-camara.vercel.app/");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
