7487c3b5c6223b7e99a300558491cab49b742d77
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 16:56:56 -0300

    update agent

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://projeto-camara.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
