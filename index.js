c675120e7b3b9828a43c0927d6c0e47306b266b2
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 14:46:42 -0300

    update agent

--- index.js
+++ index.js
@@ -6,32 +6,24 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
-const allowedOrigins = [
-    'http://localhost:4200',
-    'https://api-camara.vercel.app',
-    'https://projeto-camara.vercel.app/'
-];
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Origin not allowed by CORS'));
-        }
-    },
-};
-app.options('*', cors(corsOptions));
-
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors({
+    origin: '*',
+    allowedHeaders: [
+        'Access-Control-Allow-Origin: https://api-camara.vercel.app',
+        'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE',
+        'Access-Control-Allow-Headers: *'
+    ]
+}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(cookieParser());
 
 const router = require('./src/routes');
 app.use(router);
-app.use('/uploads', cors(corsOptions), express.static('uploads'));
+app.use('/uploads', cors(), express.static('uploads'));
 
-app.use('/', cors(corsOptions), router);
+app.use('/', cors(), router);
 
 app.listen(port);
 console.log('API funcionando!');
