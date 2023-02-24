f04c226fca0bd70873390273e517b02d81d4f5c2
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 16:05:20 -0300

    update agent

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
 
-let allowCors = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-}
-
-app.use(allowCors);
+app.use(cors());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cookieParser());
