365d174a9404d0055e078237add15992392808b2
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 14:10:22 -0300

    update agent

--- index.js
+++ index.js
@@ -7,28 +7,17 @@ const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
 
-app.use(cors({origin: '*'}));
+app.use(cors({
+    origin: '*',
+    allowedHeaders: [
+        'Access-Control-Allow-Origin: https://projeto-camara.vercel.app',
+        'Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE',
+        'Access-Control-Allow-Headers: *'
+    ]
+}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 const router = require('./src/routes');
 app.use(router);
