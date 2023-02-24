155fe6ea4a3327469d4a7d8a3efbca0c2a172779
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 14:59:49 -0300

    update agent

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
 
-app.use(cors());
+let allowCors = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "https://projeto-camara.vercel.app/");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCors);
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cookieParser());
