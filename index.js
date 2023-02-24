292eaf94e9af7cfb1ee8c3a15071b0b8f3db92db
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 12:58:33 -0300

    update agent

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.use(cors({
     origin: ['*', 'https://projeto-camara.vercel.app/'],
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Origin", "https://projeto-camara.vercel.app/"); 
+
+    res.header('Access-Control-Expose-Headers', 'agreementrequired');
+  
+    next();
+})
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cookieParser());
