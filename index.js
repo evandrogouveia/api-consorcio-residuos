702973831bef577e441eb8957a0295af43ab6af2
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 18:08:32 -0300

    update agent

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://projeto-camara.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
@@ -33,9 +14,11 @@ app.use(cookieParser());
 
 const router = require('./src/routes');
 app.use(router);
-app.use('/uploads', cors(), express.static('uploads'));
+app.use('/uploads', express.static('uploads'), (req, res) => {
+    res.setHeader('Content-Type', 'image/*');
+});
 
-app.use('/', cors(), router);
+app.use('/', router);
 
 app.listen(port);
 console.log('API funcionando!');
