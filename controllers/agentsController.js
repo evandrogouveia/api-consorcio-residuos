5d5bc4d370d29921f4d6244b4062b98cc746f192
vandogouveia <vando_gouveia@hotmail.com>
2023-02-24 17:26:20 -0300

    update agent

--- controllers/agentsController.js
+++ controllers/agentsController.js
@@ -17,7 +17,21 @@ module.exports = {
     }),
 
     newAgent(req, res) {
-        
+        // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://projeto-camara.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
         let dataForm = JSON.parse(req.body.formAgent);
         const photo = req.files[0]?.filename ? `${process.env.BASE_URL}/uploads/agents/${req.files[0]?.filename}` : '';
         const name = dataForm.name;
