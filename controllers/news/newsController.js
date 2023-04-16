d4b62c2c4bfce47a405db1e3b8b83726d59413ee
vandogouveia <vando_gouveia@hotmail.com>
2023-04-16 15:23:07 -0300

    multer tostring

--- controllers/news/newsController.js
+++ controllers/news/newsController.js
@@ -12,7 +12,7 @@ module.exports = {
             cb(null, path);
         },
         filename: function (req, files, cb) {
-            cb(null, `${Date.now()}-${files.originalname}`);
+            cb(null, `${Date.now()}-${files.originalname.toString()}`);
         }
     }),
 
