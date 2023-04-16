396a4d706a4a19b05e5e2b1f24d18828d5f0831e
vandogouveia <vando_gouveia@hotmail.com>
2023-04-16 13:04:49 -0300

    option in static

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ app.use(cookieParser());
 
 const router = require('./src/routes');
 app.use(router);
-app.use('/uploads', cors(), express.static('uploads'));
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
+app.use('/uploads', cors(), express.static('uploads', options));
 
 app.use('/api-camara/', cors(), router);
 //app.use('/', cors(), router);
