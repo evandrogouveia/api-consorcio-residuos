f30170eb95a2a00a2aa50724aa4023ef62a76780
vandogouveia <vando_gouveia@hotmail.com>
2023-04-24 12:03:43 -0300

    ajustes index

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const dotenv = require('dotenv');
 var cors = require('cors');
 dotenv.config();
 
-app.use(cors({
-    origin: ['https://cesistemaslegislativo.com.br'],
-    "methods": "GET,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    credentials: true
-}));
+app.use(cors({origin: '*'}));
 
 app.use(bodyParser.json({ limit: '250mb' }));
 app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }));
@@ -24,8 +18,8 @@ app.use(router);
 
 app.use('/uploads', express.static('uploads'));
 
-app.use('/api-camara/', router);
-//app.use('/', cors(), router);
+//app.use('/api-camara/', router);
+app.use('/', cors(), router);
 
 app.listen(port);
 console.log('API funcionando!');
